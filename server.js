a1b0cd0115d66a41cace2f2cf5f07e6a295ee6f5
coyote <megan.metelak@gmail.com>
2022-02-23 12:39:05 -0600

    copy server.js snapshot order

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+// connect/use express-session and sequelize store
+const session = require('express-session');
 // connecting handlebars as template engine of choice
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-// connect/use express-session and sequelize store
-// const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'dancing umbrellas',
   cookie: {},
@@ -20,35 +20,40 @@ const sess = {
     db: sequelize
   })
 };
+
 app.use(session(sess));
+
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // turn on path to public folder - stylesheets
 app.use(express.static(path.join(__dirname, 'public')));
-// turn on routes
-app.use(routes);
+
+app.use(require('./controllers/'));
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
 
-
-// const path = require('path');
 // const express = require('express');
-// // connect/use express-session and sequelize store
 // const session = require('express-session');
-// // connecting handlebars as template engine of choice
-// const exphbs = require('express-handlebars');
-
+// const routes = require('./controllers/');
+// const sequelize = require('./config/connection');
+// const path = require('path');
 // const app = express();
 // const PORT = process.env.PORT || 3001;
-
-// const sequelize = require('./config/connection');
+// // connecting handlebars as template engine of choice
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+// // connect/use express-session and sequelize store
+// // const session = require('express-session');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 // const sess = {
 //   secret: 'dancing umbrellas',
 //   cookie: {},
@@ -58,24 +63,15 @@ sequelize.sync({ force: false }).then(() => {
 //     db: sequelize
 //   })
 // };
-
 // app.use(session(sess));
-
-// const hbs = exphbs.create({});
-
 // app.engine('handlebars', hbs.engine);
 // app.set('view engine', 'handlebars');
-
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // // turn on path to public folder - stylesheets
 // app.use(express.static(path.join(__dirname, 'public')));
-
 // // turn on routes
 // app.use(routes);
-
-// const routes = require('./controllers/');
-
 // // turn on connection to db and server
 // sequelize.sync({ force: false }).then(() => {
 //   app.listen(PORT, () => console.log('Now listening'));
